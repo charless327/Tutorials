6f1eab53b53c5f3e7546bf5d433e71b5f6af9e32
Si Book <sibook@tests-MacBook.local>
2022-06-27 17:27:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/tutorials/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.android.tutorials
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnOpenActivity.setOnClickListener {
+            Intent(this,SecondActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+
+
+
+        }
+    }
